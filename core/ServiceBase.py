084b159fab1cafad4bbda7d0fc22880fb563407d
tbr0117 <bhargavreddy033@gmail.com>
2021-12-28 23:49:12 +0530

    added new methods

--- core/ServiceBase.py
+++ core/ServiceBase.py
@@ -1,6 +1,8 @@
 import pyodata
 import requests
 from pyodata.v2.service import GetEntitySetFilter
+from requests.models import Response
+
 
 class ServiceBase:
     def __init__(self, sServce_url, sClent):
@@ -8,25 +10,29 @@ class ServiceBase:
             sClent = "510"
 
         params = {"sap-client": sClent}
-        session = requests.session()
-        session.params = params
-        self.service_instance = pyodata.Client(sServce_url, session)
+        self._service_url = sServce_url
+        self._session = requests.session()
+        self._session.params = params
+        self._service_instance = pyodata.Client(sServce_url, self._session)
+
+    def getEntitySet(self, sEntityName):
+        return self._service_instance.entity_sets._entity_sets[sEntityName]
 
     def readEntitySet(self, oRequest={}):
-        sEntityName = oRequest.get("EntityName", "")
+        sEntityName = oRequest.get("entityName", "")
         if not sEntityName:
             return
-        entity_set_request = self.service_instance.entity_sets._entity_sets[sEntityName].get_entities()
-        
+        entity_set_request = self.getEntitySet(sEntityName).get_entities()
+
         sSelect = oRequest.get("select", "")
         sFilter = oRequest.get("filter", "")
 
         if sSelect:
             entity_set_request.select(sSelect)
-        
+
         if sFilter:
             entity_set_request.filter(sFilter)
-        
+
         entity_set_request.skip(oRequest.get("skip", 0))
         entity_set_request.top(oRequest.get("top", None))
         sCount = oRequest.get("count", None)
@@ -37,7 +43,8 @@ class ServiceBase:
 
         result = entity_set_request.execute()
 
-        final_result = dict(count=len(result), totalCount=result.total_count, data=[])
+        final_result = dict(count=len(result),
+                            totalCount=result.total_count, data=[])
         for r in result:
             record = dict(r._cache.items())
             final_result["data"].append(record)
@@ -45,4 +52,46 @@ class ServiceBase:
         return final_result
 
     def readEntity(self, oRequest={}):
+        if not oRequest["entityName"]:
+            return
+        if not oRequest["entityKey"]:
+            return
+
+        read_request = self.getEntitySet(
+            oRequest["entityName"]).get_entity(oRequest["entityKey"])
+
+        sSelect = oRequest.get("select", "")
+        sExpand = oRequest.get("expand", "")
+        if sSelect:
+            read_request.select(sSelect)
+        if sExpand:
+            read_request.expand(sExpand)
+        oResponse = read_request.execute()
+        print(oRequest)
+
+    def createEntity(self, oRequest={}):
+        if not oRequest["entityName"]:
+            return
+        if not oRequest["data"]:
+            return
+
+        create_request = self.getEntitySet(
+            oRequest["entityName"]).create_entity()
+        create_request.set(**oRequest["data"])
+        oResponse = create_request.execute()
+        print(oResponse)
+
+    def updateEntity(self, oRequest={}):
         pass
+
+    def deleteEntity(self, oRequest={}):
+        pass
+
+    def fetchCSRFToken(self):
+        sToken = self._session.headers.get("x-csrf-token", "")
+        if not sToken:
+            oResponse = self._session.head(self._service_url, headers={
+                                           'x-csrf-token': 'fetch'})
+            sToken = oResponse.headers.get('x-csrf-token', '')
+            self._session.headers.update({'x-csrf-token', sToken})
+            self._service_instance.http_get()
--- main.py
+++ main.py
@@ -2,16 +2,34 @@ from core.ServiceBase import ServiceBase
 
 service_url = 'http://services.odata.org/V2/Northwind/Northwind.svc/'
 service_base = ServiceBase(service_url, '510')
-oRequest = {
-    "EntityName": "Customers",
-    "select": "CustomerID, CompanyName, ContactTitle, Country",
-    "filter": "Country eq 'Germany'",
-    "count": "inline",
-    "top": 20
-}
-Customers_response = service_base.readEntitySet(oRequest)
-# Customer = service_base.readEntity()
 
-for employee in Customers_response.get("data", []):
-    if employee:
-        print(employee)
+
+def get_entity_set():
+    oRequest = {
+        "entityName": "Customers",
+        "select": "CustomerID, CompanyName, ContactTitle, Country",
+        "filter": "Country eq 'Germany'",
+        "count": "inline",
+        "top": 20
+    }
+    Customers_response = service_base.readEntitySet(oRequest)
+    # Customer = service_base.readEntity()
+
+    for employee in Customers_response.get("data", []):
+        if employee:
+            print(employee)
+
+
+def read_entity():
+    oRequest = {
+        "entityName": "Customers",
+        "entityKey": "BLONP",
+        "expand": "Orders"
+    }
+    customer_response = service_base.readEntity(oRequest)
+    print(customer_response)
+
+
+get_entity_set()
+
+read_entity()
